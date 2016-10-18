a69490b4d26030b75b83623543e810beb52481f1
Kristen Harrison <kgharrison1@gmail.com>
2016-10-18 17:10:09 -0500

    added additional tracker to allow players to take turns going first

--- intermediate_ruby/tictactoe.rb
+++ intermediate_ruby/tictactoe.rb
@@ -14,52 +14,63 @@ class Game
 		@grid = [0,1,2,3,4,5,6,7,8]
 
 		# initializes turn variable
-		@current_turn = 1
+		@turns = 0
+
+		# produces 0 or 1 randomly to decide which player goes first
+		@tracker = rand(2)
 	end
 
-	# outputs array to screen with three indices in each row
-	def show_grid
-		puts @grid.each_slice(3) { |row| puts row.join(" . ")}
+
+	# plays until a win or the board is full
+	def play
+		while !win && @turns < 9
+			turn
+		end
+		result
 	end
 
-	# identifies which player's turn it is to play
-	def current_player
-		@current_turn.even? ? @player_O : @player_X
-	end
-
-	# identifies which player just played, to check if won
-	def played_last
-		@current_turn.even? ? @player_X : @player_O
-	end
 
 	# handles the input for each turn
 	def turn
 		# Queries current player for move and displays grid
-		puts "\n#{current_player.name}, it's your turn. Where would you like to go?\n"
+		puts "\n#{current_player.name}, it's your turn. Where would you like to go?\n\n"
 		show_grid
 
 		# checks if move is valid and updates grid array
 		move = gets.chomp.to_i
 		if move.between?(0, 8) && @grid.include?(move)
 			@grid[move] = current_player.symbol
-			@current_turn += 1	
+			@turns += 1	
+			@tracker += 1
 		else
 			puts "Invalid move. Please type the number at which you would like to place your #{current_player.symbol}"
 		end
 	end
+
+
+	# outputs array to screen with three indices in each row
+	def show_grid
+		puts @grid.each_slice(3) { |row| puts row.join(" . ")}
+	end
+
+
+	# identifies which player's turn it is to play
+	def current_player
+		@tracker.even? ? @player_O : @player_X
+	end
+
+
+	# identifies which player just played, to check if won
+	def played_last
+		@tracker.even? ? @player_X : @player_O
+	end
+
 
 	# evaluates true if any winning combos were played by the last player
 	def win
 		WIN.any? {|combo| combo.all? { |i| @grid[i] == played_last.symbol} }
 	end
 
-	# plays until a win or the board is full
-	def play
-		while !win && @current_turn < 10
-			turn
-		end
-		result
-	end
 
 	# shows game over results; tie or win
 	def result
@@ -73,18 +84,19 @@ class Game
 		play_again
 	end
 
+
+	# offers rematch
 	def play_again
 		puts "\nWould you like to play again? y/n"
 		answer = gets.chomp.downcase
 
 		if answer == "y"
-			@grid = [0,1,2,3,4,5,6,7,8]
-			@current_turn = 1
-			play
+			Game.new.play
 		else
-			puts "\nThanks for playing!\n"
+			puts "\nThanks for playing!\n\n"
 		end
 	end
+
 end
 
 
