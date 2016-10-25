4858b23da7bdc3c4fd357b650776f963cfead1c4
Kristen Harrison <kgharrison1@gmail.com>
2016-10-25 12:48:19 -0500

    finished functionality, refactored

--- intermediate_ruby/mastermind.rb
+++ intermediate_ruby/mastermind.rb
@@ -6,34 +6,39 @@ module Mastermind
 		def initialize
 			create_code
 			@turns = 1
-		end
-=begin			puts %Q{
-nWelcome to Mastermind! The computer has generated a 4-digit code 
+			@game_over = false
+
+			puts %Q{
+\nWelcome to Mastermind! The computer has generated a 4-digit code 
 made of numbers between 0 and 5. For each guess you make, the program 
-will give you a clue with numbers indicating how correct 
-you were. A 0 means no matches, a 1 means correct number but in the 
-wrong space, and a 2 means correct number and place. The placement 
-of these number clues do not correlate to the placement of numbers in 
-our guess. You have 12 guesses! Please begin.\n\n}
-=end
+will give you a clue with numbers indicating how correct you were.
+A 1 means correct number but in the wrong space, and a 2 means correct 
+number and place. The placement of these number clues do not correlate 
+to the placement of numbers in our guess. You have 12 guesses! 
+Please begin.\n\n}
+			end
 
 
 		def create_code
 			# creates random 4-digit code with numbers from 0 to 5
-			#@code = Array.new(4) {|index| rand(6)}
-			@code = [1,2,1,2]
+			@code = Array.new(4) {|index| rand(6)}
+			#@code = [1,2,1,2]
 		end
 
 		def play
-			while @turns < 13
+			while @turns < 12 && !@game_over
 				turn
+			end
+			if @turns == 12
+				puts "\nYou ran out of turns :/ \n"
+				play_again
 			end
 		end
 
 
 
 		def turn
-			# turns guess input into array
+			# turns user's guess input into array
 			guess = gets.chomp.split(//).map {|s| s.to_i }
 			# error checking
 			if guess.all? {|i| i.between?(0, 5)}
@@ -47,9 +52,9 @@ our guess. You have 12 guesses! Please begin.\n\n}
 
 
 		def check_code(guess)
+			#initializes array to build clue
 			clue = []
-			# keeps track of which indices have been matched
-			@indices = [0,1,2,3]
+
 			exact_matches = exact_match(guess)
 			# adds a 2 for each exact match
 			exact_matches.times { clue << 2 } 
@@ -57,17 +62,26 @@ our guess. You have 12 guesses! Please begin.\n\n}
 			semi_matches = semi_match(guess)
 			# adds a 1 for each "semi-match"
 			semi_matches.times { clue << 1 }
+
+			# redirect to won if all values are correct
+			if clue == [2,2,2,2]
+				@game_over = true	
+				won
 			# gives feedback to user about the accuracy of their guess
-			puts clue.empty? "0 matches" : clue.to_s
+			else 
+				puts clue.empty? ? "0 matches" : clue.join.to_s
+			end
 		end
 
 
 		# returns the number of exact matches
 		def exact_match(guess)
-			# deletes values in @indexes of the matching indices in guess and @code
-			@indices.each {|i| @indices.delete(i) if guess[i] == @code[i] }
-			# sums @indices and returns total subtracted from 4 
-			return 4 - @indices.length
+			# keeps track of which indices have been matched
+			@indices = [0,1,2,3]
+			# creates array of the matching indices in guess and @code
+			@indices.delete_if {|i| guess[i] == @code[i] }
+			# returns number of exact matches
+			4 - @indices.length
 		end
 
 		# finds total number of "semi-matches" (correct number, incorrect index)
@@ -81,6 +95,24 @@ our guess. You have 12 guesses! Please begin.\n\n}
 			}
 			# returns number of correct numbers in the incorrect place
 			@indices.length - unmatched.length
+		end
+
+		def won
+			puts "\nYou won!!!\n\n"
+			play_again
+		end
+
+		def play_again
+			puts "Would you like to play again? y/n"
+			answer = gets.chomp.downcase
+			if answer == "y"
+				@turns = 1
+				@game_over = false
+				create_code
+				play
+			else
+				puts "Thank you for playing!\n"
+			end
 		end
 	end
 
