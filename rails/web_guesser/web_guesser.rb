154f95356a40c386d548e1cce1b767c65486e748
Kristen Harrison <kgharrison1@gmail.com>
2016-11-17 11:05:55 -0600

    added method to check guess and displays number at end

--- rails/web_guesser/web_guesser.rb
+++ rails/web_guesser/web_guesser.rb
@@ -1,6 +1,30 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-get '/' do 
-	"The secret number is #{rand(101)}"
+
+NUMBER = rand(101)
+
+
+def check_guess(guess)
+	if guess == nil
+		message = "Please guess a number between 0 and 100"
+	elsif guess > NUMBER 
+		message = (guess - NUMBER < 10)? "Too high!" : "Way too high!"
+	elsif guess < NUMBER
+		message = (NUMBER - guess < 10)? "Too low!" : "Way too low!"
+	elsif guess == NUMBER
+		message = "You guessed correctly!"
+	end
 end
+
+
+get '/' do 
+	guess = params['guess'].to_i unless params['guess'] == nil
+	
+	message = check_guess(guess)
+	secret = "The secret number is #{NUMBER}" if guess == NUMBER
+
+	erb :index, :locals => {:message => message, :secret => secret}
+end
+
+
