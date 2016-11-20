7798a97b2b3af517de01833b12a4cb2df6d05964
Kristen Harrison <kgharrison1@gmail.com>
2016-11-20 14:28:23 -0600

    hangman app modified for sinatra

---
+++ rails/hangman/hangman.rb
@@ -0,0 +1,109 @@
+	require 'sinatra'
+	require 'sinatra/reloader' if development?
+
+	class Game
+		# variable to set total number of turns
+		TURNS = 15
+
+
+		def initialize
+			pick_word
+			build_outline
+			@turns = 0
+		end
+
+
+		def pick_word
+			# reads in words from text file, selects the ones between 5-12 letters
+			words = File.readlines("wordlist.txt").select do |word| 
+				word.strip.length.between?(5, 12) 
+			end
+			# pick random word from words array
+			@secret_word = words[rand(words.size)].strip.downcase
+		end
+
+
+		def build_outline
+			# builds string of underscores to store progression of guesses
+			@guess_word = ""
+			@secret_word.length.times { @guess_word << "_" }
+		end
+
+
+		# plays until a win or the player runs out of turns
+		def play
+			while !win && @turns < TURNS
+				turn
+			end
+			# counter increments at start of turn, so must be less than max turns
+			if win
+				play_again
+			else
+				puts "\n Sorry, you ran out of turns. The word was #{@secret_word}"
+				play_again
+			end
+		end
+
+
+		# accepts one character from user and checks for matches and win
+		def turn
+			@turns += 1
+			@guess_letter = gets[0].downcase
+			show_matches
+			check_win
+		end
+
+
+		def show_matches
+			# iterate through secret word and sub in accurate guesses
+			@secret_word.each_char.with_index do |char, index|
+				@guess_word[index] = char if char == @guess_letter 
+			end
+			# prints updated guess with matched letters
+			@guess_word.each_char { |char| print " #{char}" }
+		end
+
+	
+		# win when cumulative guesses equal the secret word
+		def win
+			@guess_word == @secret_word
+		end
+
+
+		# shows win or remaining turns
+		def check_win
+			if win
+				puts "\n You win!" 
+			else
+				# shows how many turns remain
+				puts "\n You have #{TURNS - @turns} turns left"
+			end
+		end
+
+
+		# asks user if rematch desired
+		def play_again
+			puts "\n Would you like to play again? y/n"
+			answer = gets.chomp.downcase
+
+			if answer == ("y" || "yes")
+				# Game.new.play
+			else
+				puts "\n Thank you for playing!"
+			end
+		end
+
+	end
+
+
+	get '/' do 
+		# guess set to "" if params are empty to avoid nil exception
+		guess = params['guess'] ||= ""
+		
+		guesstext = guess
+		turns = 15
+		# guesstext = caesar_cipher(guess, cipher)
+		erb :index, :locals => {:guesstext => guesstext, :turns => turns}
+	end
+
+
