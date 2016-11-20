4d39f8d99c09470d6e5db295c804ec9e985b21c5
Kristen Harrison <kgharrison1@gmail.com>
2016-11-20 12:04:05 -0600

    simple app encrypts a string

--- rails/caesar_cipher/cipher.rb
+++ rails/caesar_cipher/cipher.rb
@@ -3,27 +3,29 @@ require 'sinatra/reloader'
 
 # determines alphabetic case, prints if non-letter
 def caesar_cipher(text, cipher)
-  text.scan(/./) do |char|
+  encrypted = text.split('').map do |char|
   	if char.between?("a", "z")
-	  	letter_shift(char, cipher, 122)
+	  	char = letter_shift(char, cipher, 122)
 	elsif char.between?("A", "Z")
-	  	letter_shift(char, cipher, 90)
-	 else 
-	 	return char
-	 end
+	  	char = letter_shift(char, cipher, 90)
+	else 
+	 	char
+	end
   end
+  return encrypted.join
 end
 
 
-def letter_shift(char, cipher, z_int)
+def letter_shift(char, cipher, z_code)
   ascii = char.ord + cipher%26
-  ascii-=26 if ascii > z_int 
+  ascii-=26 if ascii > z_code 
   return ascii.chr
 end
 
 
 get '/' do 
-	plaintext = params['plaintext']
+	# plaintext set to "" if params are empty to avoid nil exception
+	plaintext = params['plaintext'] ||= ""
 	cipher = params['cipher'].to_i
 	
 	ciphertext = caesar_cipher(plaintext, cipher)
